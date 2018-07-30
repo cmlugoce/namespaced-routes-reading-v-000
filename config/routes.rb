ba0b7aab32f47becafabf36765c3ecaaf0bbc726
Cristina Lugo <cmlugoce@syr.edu>
2018-07-30 16:10:10 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :posts, only: [:show, :index, :new, :edit]
   end
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-  scope '/admin', module: 'admin' do
+  namespace :admin do
   resources :stats, only: [:index]
 end
   root 'posts#index'
