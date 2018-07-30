e17d79f96f93bb212be023b4a32332524d8ab41d
Cristina Lugo <cmlugoce@syr.edu>
2018-07-30 16:02:46 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new, :edit]
   end
-
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-
+get '/stats', to: 'stats#index'
   root 'posts#index'
 end
